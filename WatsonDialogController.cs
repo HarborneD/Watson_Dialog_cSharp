bb67c801cfc49067aa425ab1f00b2935bcbd547b
HarborneD <d.harborne@outlook.com>
2016-10-11 22:25:52 +0100

    Initial Commit

---
+++ WatsonDialogController.cs
@@ -0,0 +1,258 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using Watson_Dialog.Helpers;
+
+namespace Watson_Dialog.Controllers
+{
+    public class WatsonDialogController : Controller
+    {
+        // GET: WatsonDialog
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //HttpPostedFileBase
+
+        //Dialogs
+        public ActionResult Dialogs(string msg ="")
+        {
+            watson_dialog_tool dialog_tool = new watson_dialog_tool();
+
+            watson_dialog_API_Reply dialogs = dialog_tool.get_dialogs();
+
+            List<watson_dialog_dialog> dialog_list = new List<watson_dialog_dialog>();
+
+            if (dialogs.success)
+            {
+                dynamic json_dialog_list = JObject.Parse(dialogs.response_json);
+
+                foreach(Object o in json_dialog_list.dialogs)
+                {
+                    dialog_list.Add(JsonConvert.DeserializeObject<watson_dialog_dialog>(o.ToString()));
+                }
+            }
+
+            ViewBag.msg = msg;
+            return View(dialog_list);
+          
+        }
+
+        public ActionResult CreateDialog()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateDialog([Bind(Include = "name")] watson_dialog_dialog dialog, HttpPostedFileBase XML)
+        {
+
+            string fileName = Path.GetFileName(XML.FileName);
+
+            byte[] bytes = null;
+
+            using (Stream inputStream = XML.InputStream)
+            {
+                MemoryStream memoryStream = inputStream as MemoryStream;
+                if (memoryStream == null)
+                {
+                    memoryStream = new MemoryStream();
+                    inputStream.CopyTo(memoryStream);
+                }
+
+                bytes = memoryStream.ToArray();
+            }
+
+
+            watson_dialog_tool dialog_tool = new watson_dialog_tool();
+
+            watson_dialog_API_Reply create_response = dialog_tool.create_dialog(dialog.name, fileName,bytes);
+
+            string create_message = "";
+            if (create_response.success)
+            {
+                create_message = dialog.name + " was created successfully.";
+            }
+            else
+            {
+                create_message = "There was an error create dialog " + dialog.name + " - " + create_response.msg;
+            }
+
+            return RedirectToAction("Dialogs", new { msg = create_message });
+
+         
+        }
+
+
+
+        public ActionResult UpdateDialog(string dialog_id, string name)
+        {
+            watson_dialog_dialog dialog = new watson_dialog_dialog() { dialog_id = dialog_id, name = name };
+
+            return View(dialog);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult UpdateDialog([Bind(Include = "dialog_id")] watson_dialog_dialog dialog, HttpPostedFileBase XML)
+        {
+
+            string fileName = Path.GetFileName(XML.FileName);
+
+            byte[] bytes = null;
+
+            using (Stream inputStream = XML.InputStream)
+            {
+                MemoryStream memoryStream = inputStream as MemoryStream;
+                if (memoryStream == null)
+                {
+                    memoryStream = new MemoryStream();
+                    inputStream.CopyTo(memoryStream);
+                }
+
+                bytes = memoryStream.ToArray();
+            }
+
+
+            watson_dialog_tool dialog_tool = new watson_dialog_tool();
+            
+
+            watson_dialog_API_Reply update_response = dialog_tool.update_dialog(dialog.dialog_id, fileName, bytes);
+
+            string update_message = "";
+            if (update_response.success)
+            {
+                update_message = dialog.dialog_id + " was updated successfully.";
+            }
+            else
+            {
+                update_message = "There was an error updating dialog "+dialog.dialog_id + " - " + update_response.msg;
+            }
+            
+            return RedirectToAction("Dialogs", new {msg = update_message });
+        }
+
+        public ActionResult DeleteDialog(string dialog_id, string name)
+        {
+            watson_dialog_dialog dialog = new watson_dialog_dialog() { dialog_id = dialog_id, name = name };
+
+            return View(dialog);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteDialog([Bind(Include = "dialog_id")] watson_dialog_dialog dialog)
+        {
+
+          watson_dialog_tool dialog_tool = new watson_dialog_tool();
+
+
+          watson_dialog_API_Reply delete_response = dialog_tool.delete_dialog(dialog.dialog_id);
+
+            string update_message = "";
+            if (delete_response.success)
+            {
+                update_message = dialog.dialog_id + " was deleted successfully.";
+            }
+            else
+            {
+                update_message = "There was an error deleting dialog " + dialog.dialog_id + " - " +delete_response.msg;
+            }
+
+            return RedirectToAction("Dialogs", new { msg = update_message });
+        }
+
+        //conversations
+
+        public ActionResult CreateConversation(string dialog_id)
+        {
+            watson_dialog_tool dialog_tool = new watson_dialog_tool();
+
+            watson_dialog_API_Reply create_response = dialog_tool.send_message(dialog_id);
+
+            watson_dialog_Conversation_response response = new watson_dialog_Conversation_response();
+
+            if (create_response.success)
+            {
+                dynamic json_response = JObject.Parse(create_response.response_json);
+
+                Session["dialog_id"]= dialog_id;
+                Session["conversation_id"] = json_response.conversation_id;
+                Session["client_id"] = json_response.client_id;
+
+                string[] response_list = JsonConvert.DeserializeObject<string[]>(json_response.response.ToString());
+
+                foreach (string s in response_list)
+                {
+                    if(response.response.Length > 0)
+                    {
+                        response.response += "/n";
+                    }
+                    response.response += s.ToString();
+                }
+                
+            }
+            else
+            {
+                response.response = "<There was an error sending your message>";
+            }
+
+            return RedirectToAction("ViewConversation" ,new { OpeningMessage = response.response });
+
+        }
+
+        public ActionResult ViewConversation(string OpeningMessage)
+        {
+            ViewBag.OpeningMessage = OpeningMessage;
+            return View();
+        }
+
+
+        public JsonResult SendMessage(string message)
+        {
+            
+            watson_dialog_tool dialog_tool = new watson_dialog_tool();
+
+            watson_dialog_API_Reply send_response = dialog_tool.send_message(Session["dialog_id"].ToString(),'"'+message+'"',Convert.ToInt32(Session["conversation_id"]),Convert.ToInt32(Session["client_id"]));
+
+            watson_dialog_Conversation_response response = new watson_dialog_Conversation_response();
+
+            if (send_response.success)
+            {
+
+                dynamic json_response = JObject.Parse(send_response.response_json);
+
+                 string[] response_list = JsonConvert.DeserializeObject<string[]>(json_response.response.ToString());
+
+                foreach (string s in response_list)
+                {
+                    if (response.response.Length > 0)
+                    {
+                        response.response += "/n";
+                    }
+                    response.response += s.ToString();
+                }
+            }
+            else
+            {
+                response.response = "<There was an error sending your message>";
+            }
+
+
+
+
+            return Json(response, JsonRequestBehavior.AllowGet);
+        }
+
+
+
+    }
+}
---
+++ watson_dialog_tool.cs
@@ -0,0 +1,226 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Watson_Dialog.Helpers
+{
+    class watson_dialog_tool
+    {
+        public string base_url { get; set; }
+
+        public string username { get; set; }
+        public string password { get; set; }
+        public string credentials_string { get; set; }
+
+        public string version { get; set; }
+
+        public string dialog_id { get; set; }
+
+        public int conversation_id { get; set; }
+        public int client_id { get; set; }
+
+        public Dictionary<string,string> values { get; set; }
+
+        public watson_dialog_tool()
+        {
+            this.construct_base_url();
+
+            this.username = ConfigurationManager.AppSettings["WatsonUsername"];
+            this.password = ConfigurationManager.AppSettings["WatsonPassword"];
+            this.update_credentials_string();
+
+            this.values = new Dictionary<string, string>();
+        }
+
+        private void construct_base_url()
+        {
+            this.version = ConfigurationManager.AppSettings["WatsonVersion"];
+            this.base_url = ConfigurationManager.AppSettings["WatsonPath"] + "/" + this.version + "/dialogs";
+        }
+
+        public void update_credentials_string()
+        {
+            this.credentials_string = Convert.ToBase64String(Encoding.ASCII.GetBytes(string.Format("{0}:{1}", this.username, this.password)));
+        }
+
+        public watson_dialog_API_Reply make_request(RequestType r_type = RequestType.GET, string url_extension = "", string filename = "", byte[] bytes = null)
+        {
+            using (var client = new HttpClient())
+            using (var formData = new MultipartFormDataContent())
+            {
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", this.credentials_string);
+
+
+                if (!string.IsNullOrEmpty(filename))
+                {
+                    foreach (var keyValuePair in this.values)
+                    {
+                        formData.Add(new StringContent(keyValuePair.Value), string.Format("\"{0}\"", keyValuePair.Key));
+                    }
+
+                    if ((!string.IsNullOrEmpty(filename)) && bytes != null)
+                    {
+                        formData.Add(new ByteArrayContent(bytes), '"' + "file" + '"', "\"@" + filename + '"');
+                    }
+                }
+
+                HttpResponseMessage response = null;
+                switch (r_type)
+                { 
+                    case RequestType.POST:
+
+
+                        if (string.IsNullOrEmpty(filename))
+                        {
+                            var data = new FormUrlEncodedContent(this.values);
+                            response = client.PostAsync(this.base_url + url_extension, data).Result;
+                        }
+                        else
+                        {
+                            response = client.PostAsync(this.base_url + url_extension, formData).Result;
+                        }
+
+                        
+                        break;
+
+                    case RequestType.PUT:
+
+                        response = client.PutAsync(this.base_url + url_extension, formData).Result;
+                        break;
+
+                    case RequestType.DELETE:
+
+                        response = client.DeleteAsync(this.base_url + url_extension).Result;
+                        break;
+                
+                    default:
+                        response = client.GetAsync(this.base_url + url_extension).Result;
+                        break;
+                }
+
+                var result = response.Content.ReadAsStringAsync().Result;
+                if (!response.IsSuccessStatusCode)
+                {
+                    
+                    
+                    return new watson_dialog_API_Reply() { success = false, msg = response.ReasonPhrase };
+                }
+                else
+                {
+                    return new watson_dialog_API_Reply() { success = true,  response_json = result };
+                }
+
+               
+
+            }
+        }
+
+        #region dialogs
+
+        public watson_dialog_API_Reply get_dialogs()
+        {          
+            return this.make_request();
+        }
+
+        public watson_dialog_API_Reply create_dialog(string name,string filename, byte[] bytes)
+        {
+            this.values.Add("name", name);
+            return this.make_request(RequestType.POST,"",filename,bytes);
+        }
+
+        public watson_dialog_API_Reply update_dialog(string dialog_id,string filename, byte[] bytes)
+        {
+            this.dialog_id = dialog_id;
+
+            return this.make_request(RequestType.PUT,"/" + this.dialog_id, filename, bytes);
+        }
+
+        public watson_dialog_API_Reply delete_dialog(string dialog_id)
+        {
+            this.dialog_id = dialog_id;
+
+            return this.make_request(RequestType.DELETE, "/" + this.dialog_id);
+        }
+        #endregion dialogs
+
+        //conversations
+        public void get_convo_history()
+        {
+
+        }
+
+        public watson_dialog_API_Reply send_message(string dialog_id,string message = "hi", int conversation_id=0,int client_id =0)
+        {
+            this.dialog_id = dialog_id;
+
+            if(conversation_id > 0 && client_id > 0)
+            {
+                this.conversation_id = conversation_id;
+                this.client_id = client_id;
+
+                this.values.Add("conversation_id", this.conversation_id.ToString());
+                this.values.Add("client_id", this.client_id.ToString());
+
+            }
+
+            this.values.Add("input", message);
+            
+            return make_request(RequestType.POST,"/" + this.dialog_id + "/conversation");
+        }
+
+        public void get_client_vars()
+        {
+
+        }
+
+  
+
+    }
+
+    public class watson_dialog_API_Reply
+    {
+        public bool success { get; set; }
+        public string msg { get; set; }
+        public string response_json { get; set; } 
+    }
+
+    public class watson_dialog_dialog
+    {
+        public string name { get; set; }
+        public string dialog_id { get; set; }
+    }
+
+    public class watson_dialog_response
+    {
+        public List<string> response { get; set; }
+
+        public string input { get; set; }
+        public int conversation_id { get; set; }
+        public float confidence { get; set; }
+        public int client_id { get; set; }
+    }
+
+    public enum RequestType { GET, POST, PUT,DELETE };
+
+    public class watson_dialog_Conversation_response
+    {
+        public string response { get; set; }
+
+        public watson_dialog_Conversation_response()
+        {
+            response = "";
+        }
+    }
+
+
+
+}
